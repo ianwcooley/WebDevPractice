6d9a448659785366fd84e88661d6e2116e3036b9
ianwcooley <ianwcooley@gmail.com>
2022-06-27 23:13:55 -0400

    exercises from Chapter 2 of Eloquent JavaScript

---
+++ JS/EloquentJavaScript/ch2.js
@@ -0,0 +1,44 @@
+/* 
+Eloquent JavaScript Chapter 2
+Practice Problems
+*/
+
+let size, str, i, j;
+
+/* triangle */
+size = 7;
+str = "";
+for (i = 0; i < size; i++) {
+    str += "#";
+    console.log(str);
+}
+
+/* fizzbuzz */
+size = 100;
+str = "";
+for (i = 1; i <= 100; i++) {
+    if (!(i % 3))
+        str += "fizz";
+    if (!(i % 5))
+        str += "buzz";
+    if (str) {
+        console.log(str);
+        str = "";
+    } else {
+        console.log(i);
+    }
+}
+
+/* chessboard */
+size = 20;
+str = "";
+for (i = 0; i < size; i++) {
+    for (j = 0; j< size; j++) {
+        if (i % 2)
+            str += j % 2 ? " " : "#";
+        else
+            str += j % 2 ? "#" : " ";
+    }
+    console.log(str);
+    str = "";
+}
