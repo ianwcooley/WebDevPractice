db59f6e3e061ca2affb4651b931b8182af542790
ianwcooley <ianwcooley@gmail.com>
2025-02-21 16:56:36 -0600

    speed running js review starting at beginning of ESJ

--- JS/EloquentJavaScript/ch2.js
+++ JS/EloquentJavaScript/ch2.js
@@ -13,32 +13,47 @@ for (i = 0; i < size; i++) {
     console.log(str);
 }
 
-/* fizzbuzz */
-size = 100;
-str = "";
-for (i = 1; i <= 100; i++) {
-    if (!(i % 3))
-        str += "fizz";
-    if (!(i % 5))
-        str += "buzz";
-    if (str) {
-        console.log(str);
-        str = "";
-    } else {
-        console.log(i);
-    }
+// Alternatively,
+
+let row = '#';
+while (row.length < 8) {
+    console.log(row);
+    row += '#'
 }
 
-/* chessboard */
-size = 20;
-str = "";
-for (i = 0; i < size; i++) {
-    for (j = 0; j< size; j++) {
-        if (i % 2)
-            str += j % 2 ? " " : "#";
-        else
-            str += j % 2 ? "#" : " ";
-    }
-    console.log(str);
-    str = "";
-}
+// Or
+let layer = '#'
+do {
+    console.log(layer);
+    layer += '#'
+} while (layer.length < 8);
+
+// /* fizzbuzz */
+// size = 100;
+// str = "";
+// for (i = 1; i <= 100; i++) {
+//     if (!(i % 3))
+//         str += "fizz";
+//     if (!(i % 5))
+//         str += "buzz";
+//     if (str) {
+//         console.log(str);
+//         str = "";
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// /* chessboard */
+// size = 20;
+// str = "";
+// for (i = 0; i < size; i++) {
+//     for (j = 0; j< size; j++) {
+//         if (i % 2)
+//             str += j % 2 ? " " : "#";
+//         else
+//             str += j % 2 ? "#" : " ";
+//     }
+//     console.log(str);
+//     str = "";
+// }
