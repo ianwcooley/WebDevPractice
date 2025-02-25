b1ca65f88bd2ca2e83424e4a9632bbf2b05d4993
ianwcooley <ianwcooley@gmail.com>
2025-02-24 20:37:53 -0600

    Finally added the dominantDirection function that I guess I never wrote 2.5 years ago because i was lazy or something

--- JS/EloquentJavaScript/ch5.js
+++ JS/EloquentJavaScript/ch5.js
@@ -3,6 +3,9 @@ Eloquent JavaScript Chapter 5
 Practice Problems
 */
 
+// NB: include a link to scripts.js before the link to this file,
+// because this file references the SCRIPTS variable from scripts.js
+
 /* flatten */
 function flatten(arr) {
     return arr.reduce((a,b) => a.concat(b));
@@ -28,13 +31,62 @@ function forAll(array, test) {
     return !array.some(x => !test(x));
 }
 
+// helper functions for dominantDirection:
+
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+        })) {
+            return script;
+        }
+    }
+    return null;
+}
+
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.find(c => c.name == name);
+        if (!known) {
+            counts.push({name, count: 1});
+        } else {
+            known.count++;
+        }
+    }
+    return counts;
+}
+
+function textScripts(text) {
+    let scripts = countBy(text, char => {
+      let script = characterScript(char.codePointAt(0));
+      return script ? script.name : "none";
+    }).filter(({name}) => name != "none");
+  
+    let total = scripts.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found";
+  
+    return scripts.map(({name, count}) => {
+      return `${Math.round(count * 100 / total)}% ${name}`;
+    }).join(", ");
+  }
+  
+  console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+
 /* dominantDirection */
 function dominantDirection(text) {
-    let characterDirection = char => {
+    let directions = countBy(text, char => {
         let script = characterScript(char.codePointAt(0));
         return script ? script.direction : "none";
-    };   
-    return(countBy(text, characterDirection).reduce((a,b) => 
-        b.count > a.count ? b : a
-    ).name);
+    }).filter(({name}) => name != "none");
+
+    let total = directions.reduce((n, {count}) => n + count, 0);
+    if (total == 0) return "No scripts found";
+
+    return directions.map(({name, count}) => {
+        return `${Math.round(count * 100 / total)}% ${name}`;
+      }).join(", ");
 }
+
+console.log(dominantDirection('英国的狗说"woof", ᠮᠣᠩᠭᠣᠯ说"тяв" چ	ڇ	ڈ'));
